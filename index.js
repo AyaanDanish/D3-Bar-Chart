a3535db55f1a7d2611cf6f22c22e10fe660cf244
AyaanDanish <57128634+AyaanDanish@users.noreply.github.com>
2023-07-22 01:31:53 +0500

    Edited index.html

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.8.5/dist/d3.min.js";
+import * as d3 from "d3";
 
 const fetchData = async () => {
   const response = await fetch(
